e70679e88775f97d0741a1701a6cee7d681a8f1a
Albu Alex-Stefan <alexmhh1234@gmail.com>
2021-05-06 21:46:41 +0300

    done

--- main.py
+++ main.py
@@ -1,48 +1,41 @@
 import sys
 
 
-class DictionaryGraph:
+class AdjacencyMatrixGraph:
     def __init__(self, n):
         """
         :param n: The number of vertices
         """
         self.vertices = n
-        self.dictionary = {}
-        self.costs = {}
-        for i in range(n):
-            self.dictionary[i] = []
+        self.graph = [[0 for i in range(n)] for j in range(n)]
 
     def is_edge(self, x, y):
-        return y in self.dictionary[x]
+        return y in self.graph[x]
 
     def add_edge(self, x, y, cost):
         if not self.is_edge(x, y):
-            self.dictionary[x].append(y)
-            self.costs[(x, y)] = cost
-            self.dictionary[y].append(x)
-            self.costs[(y, x)] = cost
-
-    def parse_x(self):
-        return self.dictionary.keys()
+            self.graph[x][y] = cost
+            self.graph[y][x] = cost
 
     def prims_algorithm(self, starting_node):
-        selected = [0] * self.vertices
+        selected = [False] * self.vertices
         no_edge = 0
         selected[starting_node] = True
         print("  Edge  : Weight")
         while no_edge < self.vertices - 1:
             minimum = sys.maxsize
-            x = 0
-            y = 0
+            source_node = 0
+            destination_node = 0
             for i in range(len(selected)):
-                if selected[i] and len(self.dictionary[i]) > 0:
+                if selected[i]:
                     for j in range(len(selected)):
-                        if not(selected[j]) and j in self.dictionary[i]:
-                            if minimum > self.dictionary[i][j]:
-                                x = i
-                                y = j
-            print(str(x) + "-" + str(y) + ":" + str(self.dictionary[x][y]))
-            selected[y] = True
+                        if not(selected[j]) and self.graph[i][j]:
+                            if minimum > self.graph[i][j]:
+                                minimum = self.graph[i][j]
+                                source_node = i
+                                destination_node = j
+            print(str(source_node) + " -  " + str(destination_node) + "  :  " + str(self.graph[source_node][destination_node]))
+            selected[destination_node] = True
             no_edge += 1
 
 
@@ -50,14 +43,13 @@ class MainProgram:
     def __init__(self):
         file = open("input.txt", "r")
         number_of_vertices, number_of_edges = map(int, file.readline().split())
-        self.g = DictionaryGraph(int(number_of_vertices))
+        self.g = AdjacencyMatrixGraph(int(number_of_vertices))
 
         for edge in range(number_of_edges):
             x, y, cost = map(int, file.readline().split())
             self.g.add_edge(x, y, cost)
 
     def run(self):
-        print(self.g.dictionary)
         x = input("Choose the starting node: ")
         x = int(x)
         self.g.prims_algorithm(x)
